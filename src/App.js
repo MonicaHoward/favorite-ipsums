f105776019a04828af7861a2245c155c7291e526
Monica Howard <jay.m.howard@gmail.com>
2018-09-05 22:46:31 -0500

    Created Component for Navbar

--- src/App.js
+++ src/App.js
@@ -1,109 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-          <div class="container">
-            <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              <i class="fa fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                  <a class="nav-link" href="index.html">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="about.html">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="post.html">Sample Post</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.html">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
-        <header class="masthead">
-          <div class="overlay"></div>
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 col-md-10 mx-auto">
-                <div class="site-heading">
+        <Navbar/>
+        <header className="masthead">
+          <div className="overlay"></div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 col-md-10 mx-auto">
+                <div className="site-heading">
                   <h1>Clean Blog</h1>
-                  <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                  <span className="subheading">A Blog Theme by Start Bootstrap</span>
                 </div>
               </div>
             </div>
           </div>
         </header>
 
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-8 col-md-10 mx-auto">
-              <div class="post-preview">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="post-preview">
                 <a href="post.html">
-                  <h2 class="post-title">
+                  <h2 className="post-title">
                     Man must explore, and this is exploration at its greatest
                   </h2>
-                  <h3 class="post-subtitle">
+                  <h3 className="post-subtitle">
                     Problems look mighty small from 150 miles up
                   </h3>
                 </a>
-                <p class="post-meta">Posted by
+                <p className="post-meta">Posted by
                   <a href="#">Start Bootstrap</a>
                   on September 24, 2018</p>
               </div>
               <hr/>
-              <div class="post-preview">
+              <div className="post-preview">
                 <a href="post.html">
-                  <h2 class="post-title">
+                  <h2 className="post-title">
                     I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
                   </h2>
                 </a>
-                <p class="post-meta">Posted by
+                <p className="post-meta">Posted by
                   <a href="#">Start Bootstrap</a>
                   on September 18, 2018</p>
               </div>
               <hr/>
-              <div class="post-preview">
+              <div className="post-preview">
                 <a href="post.html">
-                  <h2 class="post-title">
+                  <h2 className="post-title">
                     Science has not yet mastered prophecy
                   </h2>
-                  <h3 class="post-subtitle">
+                  <h3 className="post-subtitle">
                     We predict too much for the next year and yet far too little for the next ten.
                   </h3>
                 </a>
-                <p class="post-meta">Posted by
+                <p className="post-meta">Posted by
                   <a href="#">Start Bootstrap</a>
                   on August 24, 2018</p>
               </div>
               <hr/>
-              <div class="post-preview">
+              <div className="post-preview">
                 <a href="post.html">
-                  <h2 class="post-title">
+                  <h2 className="post-title">
                     Failure is not an option
                   </h2>
-                  <h3 class="post-subtitle">
+                  <h3 className="post-subtitle">
                     Many say exploration is part of our destiny, but it’s actually our duty to future generations.
                   </h3>
                 </a>
-                <p class="post-meta">Posted by
+                <p className="post-meta">Posted by
                   <a href="#">Start Bootstrap</a>
                   on July 8, 2018</p>
               </div>
               <hr/>
-              <div class="clearfix">
-                <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+              <div className="clearfix">
+                <a className="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
               </div>
             </div>
           </div>
@@ -112,36 +88,36 @@ class App extends Component {
         <hr/>
 
         <footer>
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 col-md-10 mx-auto">
-                <ul class="list-inline text-center">
-                  <li class="list-inline-item">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 col-md-10 mx-auto">
+                <ul className="list-inline text-center">
+                  <li className="list-inline-item">
                     <a href="#">
-                      <span class="fa-stack fa-lg">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                      <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
                       </span>
                     </a>
                   </li>
-                  <li class="list-inline-item">
+                  <li className="list-inline-item">
                     <a href="#">
-                      <span class="fa-stack fa-lg">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                      <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
                       </span>
                     </a>
                   </li>
-                  <li class="list-inline-item">
+                  <li className="list-inline-item">
                     <a href="#">
-                      <span class="fa-stack fa-lg">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                      <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-github fa-stack-1x fa-inverse"></i>
                       </span>
                     </a>
                   </li>
                 </ul>
-                <p class="copyright text-muted">Copyright &copy; Your Website 2018</p>
+                <p className="copyright text-muted">Copyright &copy; Your Website 2018</p>
               </div>
             </div>
           </div>
---
+++ src/Navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+class Navbar extends Component {
+    render(){
+        return(
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                    <div className="container">
+                        <a className="navbar-brand" href="index.html">Favorite Ipsums</a>
+                        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu
+                        <i className="fa fa-bars"></i>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarResponsive">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                <a className="nav-link" href="about.html">About</a>
+                                </li>
+                                <li className="nav-item">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Ipsums
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="">Bacon Ipsum</a>
+        </div>
+                                </li>
+                                <li className="nav-item">
+                                <a className="nav-link" href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
